0fbd630f0ec9b4a64b1dc10a1ef31b93dace67c5
trantor-si <luccostajr@gmail.com>
2022-06-06 19:59:09 -0300

    feat: add sum function

---
+++ sum.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum(2, 2))
+}
+
+func sum(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func times(a int, b int) int {
+	return a * b
+}
+
+func div(a int, b int) int {
+	return a / b
+}
