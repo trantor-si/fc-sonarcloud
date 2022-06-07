c71acbdfd94e6722e49399ce8245576a3bc1afcf
trantor-si <luccostajr@gmail.com>
2022-06-06 21:29:41 -0300

    ci: :sparkles: remove lack of coverage by commenting not used statements.

--- sum.go
+++ sum.go
@@ -10,9 +10,9 @@ func sum(a int, b int) int {
 	return a + b
 }
 
-// func sub(a int, b int) int {
-// 	return a - b
-// }
+func sub(a int, b int) int {
+	return a - b
+}
 
 // func times(a int, b int) int {
 // 	return a * b
--- sum_test.go
+++ sum_test.go
@@ -9,3 +9,11 @@ func TestSum(t *testing.T) {
 		t.Error("The reult must be 5!")
 	}
 }
+
+func TestSub(t *testing.T) {
+	result := sub(5, 2)
+
+	if result != 3 {
+		t.Error("The reult must be 3!")
+	}
+}
