9c78965a68b0c1025aa843c0aa29bf19b824c758
trantor-si <luccostajr@gmail.com>
2022-06-06 21:15:17 -0300

    ci: :sparkles: remove lack of coverage by commenting not used statements.

--- sum.go
+++ sum.go
@@ -6,9 +6,9 @@ func main() {
 	fmt.Println(sum(2, 2))
 }
 
-// func sum(a int, b int) int {
-// 	return a + b
-// }
+func sum(a int, b int) int {
+	return a + b
+}
 
 // func sub(a int, b int) int {
 // 	return a - b
